0c405619a0e9a5e8f70ae924d0cfea3b216afc61
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-29 20:26:32 +0530

    logical

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-var age=23;
-var  citizenship=true;
-if(age>=21&&citizenship==true){
-   console.log("can vote");
-   
-}
-else{
-    console.log("cannot vote");
-    
-}
+var age = 23;
+var citizenship = true;
+if (age >= 21 && citizenship == true) {
+  console.log("can vote");
+} else {
+  console.log("cannot vote");
+} //can vote
+var age = 20;
+var invitationclub = true;
+if (age >= 21 || invitationclub == true) {
+  console.log("can enter club");
+} else {
+  console.log("cannot enter club");
+} //can enter club
+var ismember = false;
+var age = 70;
+if (ismember == true || age >= 65) {
+  console.log("isEligibleForDiscount");
+} else {
+  console.log("isNotEligibleForDiscount");
+} //isEligibleForDiscount
+var gpa = 3.6;
+var extracurriculars = true;
+var recomandations = false;
+if ((gpa >= 3.5 && extracurriculars == true) || recomandations == true) {
+  console.log("iseligibleforscholarship");
+} else {
+  console.log("noteligible");
+} //iseligibleforscholarship
